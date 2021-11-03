3e661cc45b84eede7d3ba9b18601c53ea4ee8bad
Philip Goforth <77207179+DrDiscoChickenShack@users.noreply.github.com>
2021-11-03 11:44:42 -0500

    first

---
+++ Classes Exercise/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Exercise
+{
+    class Car
+    {
+        public string Make;
+        public string Model;
+        public int Year;
+    }
+}
---
+++ Classes Exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car newCar = new Car()
+            {
+                Make = "Jeep",
+                Model = "Cherokee",
+                Year = 1994
+            };
+            Console.WriteLine($"{newCar.Year} {newCar.Make} {newCar.Model} ");
+        }
+    }
+}
